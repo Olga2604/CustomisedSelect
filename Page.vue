1ab3259d20c9c07d94a3d5321ee04265123a0bb2
Olga <30666069+Olga2604@users.noreply.github.com>
2019-02-10 19:47:10 +0300

    Create Page.vue

---
+++ Page.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>This is an awesome select!</h1>
+    <select-bar :selectPlaceholder="'Choose a fruit'"
+                :selectOptions="getAllFruits"
+                @chooseFruit="getChosenFruit"/>
+  </div>
+</template>
+
+<script>
+import SelectBar from '../../elements/SelectBar';
+
+
+export default {
+  components: { SelectBar },
+  ...
+  methods: {
+    getChosenProvider(value) {
+      return value;
+    },
+  },
+};
+</script>
